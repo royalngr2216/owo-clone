97ad142de7cfc45d2c41fa6f9a82139ba782b00a
royalngr2216 <royalngr2216@gmail.com>
2026-06-11 12:40:05 +0530

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -56,7 +56,7 @@ class Economy(commands.Cog):
 
             return
 
-        amount = 10000
+        amount = 100000
 
         add_cash(
             ctx.author.id,
@@ -116,7 +116,7 @@ class Economy(commands.Cog):
 
             return
 
-        amount = 100000
+        amount = 1000000
 
         add_cash(
             ctx.author.id,
